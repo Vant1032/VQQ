a9b75190cbb5b8e5291eba9f63ef1dab78ea91a2
Vant <forvant@qq.com>
2017-11-03 09:14:27 +0800

    Initial commit,本项目采用Intellij Idea开发

---
+++ src/RunApp.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Vant
+ * @version 2017/11/3 上午 9:04
+ */
+public class RunApp extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
